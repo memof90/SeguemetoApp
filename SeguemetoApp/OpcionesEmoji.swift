b7bb975157c9d423fd87f61466a9261af3104587
memo figueredo <memof80@gmail.com>
2019-01-30 11:46:23 -0500

    se agrego un archivo swift que controla los emoji y controla las frases

---
+++ SeguemetoApp/OpcionesEmoji.swift
@@ -0,0 +1,16 @@
+//
+//  OpcionesEmoji.swift
+//  SeguemetoApp
+//
+//  Created by Memo Figueredo on 1/30/19.
+//  Copyright © 2019 Memo Figueredo. All rights reserved.
+//
+
+import Foundation
+
+struct OpcionesEmoji {
+    
+    let emoji: String
+    let caption: String
+    
+}
