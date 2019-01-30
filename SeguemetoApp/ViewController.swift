98f4b9480651d78156fe6c90fd8c8b21e9b65e28
memo figueredo <memof80@gmail.com>
2019-01-30 11:44:04 -0500

    Initial Commit

---
+++ SeguemetoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeguemetoApp
+//
+//  Created by Memo Figueredo on 1/30/19.
+//  Copyright © 2019 Memo Figueredo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
